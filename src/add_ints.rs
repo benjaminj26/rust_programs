390fe768d73541c15f2fadf58f97b42d8f6bfa12
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-04-04 12:23:12 +0530

    Added add_ints.rs

---
+++ src/add_ints.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn main() 
+{
+    println!("Enter the first number: ");
+    let x = read_int32();
+    println!("Enter the second number: ");
+    let y = read_int32();
+    if x != -1 && y != -1
+    {
+        let sum = x+y;
+        println!("The sum of {} and {} is {}", x, y, sum);
+    }
+    else
+    {
+        println!("Invalid input");
+    }
+}
+
+fn read_int32() -> i32 
+{
+    let mut x = String::new();
+    io::stdin().read_line(&mut x).expect("Unable to read an input from the user");
+    let x = x.trim();
+    let x = match x.parse::<i32>()
+    {
+        Ok(x) => x,
+        Err(..) => -1
+    };
+    x
+}
