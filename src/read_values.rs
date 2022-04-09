acdb91f9b04f8e69bda2c3bfb1f4975f84ab0b82
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-04-09 20:59:19 +0530

    Improvements

--- src/read_values.rs
+++ src/read_values.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::io;
 
 pub fn to_int32() -> i32
@@ -8,85 +9,85 @@ pub fn to_int32() -> i32
     num
 }
 
-// pub fn to_uint32() -> u32
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:u32 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_uint32() -> u32
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:u32 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_float32() -> f32
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:f32 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_float32() -> f32
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:f32 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_int64() -> i64
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:i64 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_int64() -> i64
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:i64 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_uint64() -> u64
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:u64 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_uint64() -> u64
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:u64 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_float64() ->f64
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:f64 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_float64() ->f64
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:f64 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_int16() ->i16
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:i16 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_int16() ->i16
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:i16 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_uint16() ->u16
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:u16 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_uint16() ->u16
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:u16 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_int8() -> i8
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:i8 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_int8() -> i8
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:i8 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_uint8() -> u8
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:u8 = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_uint8() -> u8
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:u8 = str.trim().parse().expect("Unable to parse");
+    num
+}
 
-// pub fn to_size() -> isize
-// {
-//     let mut str = String::new();
-//     io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-//     let num:isize = str.trim().parse().expect("Unable to parse");
-//     num
-// }
+pub fn to_size() -> isize
+{
+    let mut str = String::new();
+    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
+    let num:isize = str.trim().parse().expect("Unable to parse");
+    num
+}
 
 pub fn to_usize() -> usize
 {
