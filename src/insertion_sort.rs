489eb53881acf011f66800436ab414ab14b3ac64
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-04-30 17:21:27 +0530

    Added insertion_sort.rs

---
+++ src/insertion_sort.rs
@@ -0,0 +1,53 @@
+use crate::read_values::to_usize;
+use crate::read_values::to_int32;
+
+pub fn insertion_sort_main()
+{
+    let mut vector:Vec<i32> = Vec::new();
+    println!("Enter the size of the vector:");
+    let size = to_usize();
+
+    for x in 0..size
+    {
+        println!("Enter the value of element {}:", x);
+        vector.push(to_int32());
+    }
+    insertion_sort(&mut vector);
+    println!("The sorted vector is:");
+
+    for x in vector
+    {
+        print!("{} ", x);
+    }
+    println!();
+}
+
+fn insertion_sort(vector:&mut Vec<i32>)
+{
+    let size = vector.len();
+    for x in 1..size
+    {
+        let key = vector[x];
+        let mut y = x-1;
+        let mut flag = 0;
+
+        while key < vector[y] 
+        {
+            vector[y+1] = vector[y];
+            if y == 0 
+            {
+                vector[y] = key;
+                flag = 1;
+                break;
+            }
+            else
+            {
+                y -= 1;
+            }
+        }
+        if flag == 0
+        {
+            vector[y+1] = key;
+        }
+    }
+}
