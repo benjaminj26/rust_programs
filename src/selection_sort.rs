05e3c2d30964a7d3e272360512d5ae540674e748
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-05-01 21:09:03 +0530

    Added selection_sort.rs

---
+++ src/selection_sort.rs
@@ -0,0 +1,44 @@
+use crate::read_values::to_usize;
+use crate::read_values::to_int32;
+
+pub fn selection_sort_main()
+{
+    println!("Enter the size of the vector:");
+    let size = to_usize();
+
+    let mut vector:Vec<i32> = Vec::new();
+
+    for x in 0..size
+    {
+        println!("Enter the element at index {}:", x);
+        vector.push(to_int32());
+    }
+    selection_sort(&mut vector);
+
+    println!("The vector after sorting is:");
+    for x in vector
+    {
+        print!("{} ", x);
+    }
+    println!();
+}
+
+fn selection_sort(vector:&mut Vec<i32>)
+{
+    let size = vector.len();
+    for x in 0..size-1
+    {
+        let mut smallest = x;
+        for y in (x+1)..size
+        {
+            if vector[y] < vector[smallest]
+            {
+                smallest = y;
+            }
+        }
+        
+        let temp = vector[smallest];
+        vector[smallest] = vector[x];
+        vector[x] = temp;
+    }
+}
