a942c0626493b37145c61a9344730b311db9281e
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-06-02 22:59:29 +0530

    Changes to linked_list.rs

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,44 +1,64 @@
-use std::rc::Rc;
+// use std::rc::Rc;
 use crate::read_values::to_int32;
 
 struct Node
 {
     data:i32,
-    next:Option<Rc<Node>>
+    next:Option<Box<Node>>
+}
+
+impl Node
+{
+    fn create_node(data:i32) -> Box<Node>
+    {
+        let new_node = Box::new(Node{data,next:None});
+        new_node
+    }
+    
+    fn append(node:Option<Box<Node>>, head:&mut Option<Box<Node>>, tail:&mut Option<Box<Node>>)
+    {
+
+    }
 }
 
 pub fn linked_list_main()
 {
-    let mut head:Option<Rc<Node>> = None;
-    let mut tail:Option<Rc<Node>> = None;
-    println!(
-        "
-            1.Insert new node
-            2.Delete a node from the end
-            3.Delete a node from the beginning
-            4.Display all the nodes
-            5.Exit
-            Enter your choice:
-        "
-    );
-    let choice = to_int32();
-    match choice
+    let mut head:Option<Box<Node>> = None;
+    let mut tail:Option<Box<Node>> = None;
+    loop
     {
-        1 => {
-            println!("Enter the data:");
-            let data = to_int32();
-            let new_node = create_node(data);
-            if head.is_none() && tail.is_none()
-            {
-                head = Some(new_node);
-                tail = Rc::clone(&new_node);
+        println!(
+            "
+                1.Insert new node
+                2.Delete a node from the end
+                3.Delete a node from the beginning
+                4.Display all the nodes
+                5.Exit
+                Enter your choice:
+            "
+        );
+        let choice = to_int32();
+        match choice
+        {
+            1 => {
+                println!("Enter the data:");
+                let data = to_int32();
+                let new_node = create_node(data);
+                if head.is_none() && tail.is_none()
+                {
+                    head = Some(Box::clone(&new_node));
+                    tail = Some(new_node);
+                }
+                else
+                {
+                    let temp = tail.unwrap();
+                }
             }
+            _ => {
+                println!("Invalid Input!!");
+            }   
         }
     }
+
 }
 
-fn create_node(data:i32) -> Rc<Node>
-{
-    let new_node = Rc::new(Node{data,next:None});
-    new_node
-}
