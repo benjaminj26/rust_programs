08d38d75e6b9b37efced91cd9470ecb5a7b7899e
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-07-09 16:16:56 +0530

    Added main.rs

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod linked_list;
+mod read_values;
+
+fn main()
+{
+    linked_list::linked_list_main();
+    // let x = std::rc::Rc::new(5);
+    // let x = std::cell::RefCell::new(5);
+    // println!("{}", x);
+}
