b404acc63b85a81daba826889dd00a8d683a21df
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-08-12 09:43:31 +0530

    Minor change

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,4 @@ mod read_values;
 fn main()
 {
     linked_list::linked_list_main();
-    // let x = std::rc::Rc::new(5);
-    // let x = std::cell::RefCell::new(5);
-    // println!("{}", x);
 }
--- src/prime_range.rs
+++ src/prime_range.rs
@@ -1,31 +1,36 @@
-use std::io;
 use std::f32;
+use std::env;
 
 pub fn prime_range()
 {
-    let mut str = String::new();
-    println!("Enter the Upper limit:");
-    io::stdin().read_line(&mut str).expect("Unable to read from stdin");
-    let upper_limit:i32 = str.trim().parse().expect("Unable to parse");
-
-    let mut flag:i32;
-
-    println!("\nThe prime numbers are:");
-
-    for num in 0..upper_limit + 1
+    let args:Vec<String> = env::args().collect();
+    let upper_limit:i32;
+    if args.len() <= 1
     {
-        flag = 0;
-        for x in 2..(f32::sqrt(num as f32) as i32 + 1)
+        println!("Too few arguments provided");
+    }
+    else
+    {
+        upper_limit = args[1].parse().expect("Unable to parse");
+        let mut flag:i32;
+
+        println!("\nThe prime numbers are:");
+
+        for num in 0..upper_limit + 1
         {
-            if num % x == 0
+            flag = 0;
+            for x in 2..(f32::sqrt(num as f32) as i32 + 1)
             {
-                flag = 1;
-                break;
+                if num % x == 0
+                {
+                    flag = 1;
+                    break;
+                }
             }
-        }
-        if flag == 0
-        {
-            println!("{}", num);
+            if flag == 0
+            {
+                println!("{}", num);
+            }
         }
     }
 }
