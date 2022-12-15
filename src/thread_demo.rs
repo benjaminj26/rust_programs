fc2457b444da322bd2a65431ff3a0fdc06b32813
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-12-15 21:08:42 +0530

    Updated thread_demo.rs

--- src/thread_demo.rs
+++ src/thread_demo.rs
@@ -1,19 +1,32 @@
-use std::{thread, time::Duration};
+use std::thread;
 
 pub fn thread_demo()
 {
-	let t1 = thread::spawn(|| {
-		for _ in 0..10
-		{
-			println!("From Spawned thread!");
-			thread::sleep(Duration::from_secs(2));
-		}
-	});
+    let mut thread_vector = Vec::new();
 
-	for _ in 0..10
-	{
-		println!("From Main thread!");
-		thread::sleep(Duration::from_secs(1));
-	}
-	t1.join().unwrap();
+    for x in 0..8
+    {
+        let t = thread::spawn(move || {
+            for num in 0..(100000 * (x % 2) + (x * 18))
+            {
+                for x in 2..(f32::sqrt(num as f32) as i32 + 1)
+                {
+                    if num % x == 0
+                    {
+                        break;
+                    }
+                }
+            }
+            println!("Thread {} Done", x + 1);
+        });
+
+        thread_vector.push(t);
+    }
+
+    for x in thread_vector
+    {
+        x.join().unwrap();
+    }
+
+	println!("All the threads have finished execution");
 }
